1569e3bb99dc5e01c50fb864a9de0140c5ad4a4f
bePramudya <bepramudyaa@gmail.com>
2026-07-27 17:41:40 +0700

    feat add new register endpoint and configure reset endpoint

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 type apiConfig struct {
 	fileserverHits atomic.Int32
 	db             *database.Queries
+	platform       string
 }
 
 func main() {
@@ -34,9 +35,15 @@ func main() {
 	}
 	dbQueries := database.New(dbConn)
 
+	platform := os.Getenv("PLATFORM")
+	if platform == "" {
+		log.Fatal("PLATFORM must be set")
+	}
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
 		db:             dbQueries,
+		platform:       platform,
 	}
 	mux := http.NewServeMux()
 
@@ -47,7 +54,8 @@ func main() {
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 	mux.HandleFunc("POST /api/validate_chirp", handlerChirpsValidate)
-	// mux.HandleFunc("POST /api/users", handlerRegister)
+	mux.HandleFunc("POST /api/users", apiCfg.handlerRegister)
+	// mux.HandleFunc("POST /api/chirps", apiCfg.handlerRegister)
 
 	server := &http.Server{
 		Addr:    ":" + port,
